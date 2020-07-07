d907ff152eb1e4f9ad2fe181c02589d74fc8d513
Dmitry Pautov <111112223344az@gmail.com>
2020-07-07 23:59:55 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Layout from './components/Hight/Layout';
+import Main from './components/Hight/Main';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <Layout>
+          <Main />
+        </Layout>
+      </div>
+    )
+  }
+}
